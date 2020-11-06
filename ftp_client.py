63a5e6eafc90fe5477db0362f5c1cee7e2f2e35b
aaron <kingjojo3@gmail.com>
2020-11-06 15:38:19 -0500

    Fixed connect() method
    Double parantheses are absolutely needed, as the value MUST be a tuple

--- ftp_client.py
+++ ftp_client.py
@@ -1,22 +1,23 @@
 import socket
 
 
-
-def connect(ip_address: str, port: int): 
+def connect(ip_address: str, port: int):
     try:
-        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        sckt.connect(ip_address, port)
+        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sckt.connect((ip_address, port))
         print("Connection to " + ip_address + " on port " + str(port) + " succsessful.")
+        return sckt
     except socket.error:
         print('Unable to make connection')
-    return 
+    return
+
 
 user_input = ''
 
 # Commands are CONNECT, QUIT LIST, RETR, STOR and QUIT
 
 
-while( user_input != 'QUIT' or user_input != 'Q'):
+while user_input != 'QUIT' or user_input != 'Q':
     print("""FTP Commands are as follows: 
         \n[C]onnect: 
         \n[L]ist 
@@ -24,17 +25,15 @@ while( user_input != 'QUIT' or user_input != 'Q'):
         \n[S]tor 
         \n[Q]uit:""")
     user_input = input('\n')
-    if (user_input[0:6].upper() == 'CONNECT' or user_input[0].upper() == 'C'):
-        args = user_input.split(' ') 
-        connect(args[1], int(args[2]))
-        continue1
-    elif (user_input.upper() == 'LIST' or user_input.upper() == 'L'):
-        print('L')
-    elif (user_input.upper() == 'RETR' or user_input.upper() == 'R'):
-        print('R')
-    elif (user_input.upper() == 'STOR' or user_input.upper() == 'S'): 
-        print('S')
-    elif(user_input.upper() == 'QUIT' or user_input.upper() == 'Q'):
-        break 
-
-
+    if user_input[0:6].upper() == 'CONNECT' or user_input[0].upper() == 'C':
+        args = user_input.split(' ')
+        sckt = connect(args[1], int(args[2]))
+        continue
+    elif user_input.upper() == 'LIST' or user_input.upper() == 'L':
+        sckt.send(b"LIST")
+    elif user_input.upper() == 'RETR' or user_input.upper() == 'R':
+        sckt.send(b"RETR")
+    elif user_input.upper() == 'STOR' or user_input.upper() == 'S':
+        sckt.send(b"STOR")
+    elif user_input.upper() == 'QUIT' or user_input.upper() == 'Q':
+        break

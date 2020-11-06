6f2be0362948cb0e2b9edc23464b3b4329ecde83
aaron <kingjojo3@gmail.com>
2020-11-06 14:47:41 -0500

    connect is mostly written
    won't take two arguments for some reason...

--- ftp_client.py
+++ ftp_client.py
@@ -1,7 +1,21 @@
 import socket
 
+
+
+def connect(ip_address: str, port: int): 
+    try:
+        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        sckt.connect(ip_address, port)
+        print("Connection to " + ip_address + " on port " + str(port) + " succsessful.")
+    except socket.error:
+        print('Unable to make connection')
+    return 
+
 user_input = ''
+
 # Commands are CONNECT, QUIT LIST, RETR, STOR and QUIT
+
+
 while( user_input != 'QUIT' or user_input != 'Q'):
     print("""FTP Commands are as follows: 
         \n[C]onnect: 
@@ -10,8 +24,10 @@ while( user_input != 'QUIT' or user_input != 'Q'):
         \n[S]tor 
         \n[Q]uit:""")
     user_input = input('\n')
-    if (user_input.upper() == 'CONNECT' or user_input.upper() == 'C'):
-        print('C')
+    if (user_input[0:6].upper() == 'CONNECT' or user_input[0].upper() == 'C'):
+        args = user_input.split(' ') 
+        connect(args[1], int(args[2]))
+        continue1
     elif (user_input.upper() == 'LIST' or user_input.upper() == 'L'):
         print('L')
     elif (user_input.upper() == 'RETR' or user_input.upper() == 'R'):

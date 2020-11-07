42adbb81122e8076dda6bb7411f70817feffc5e5
aaron <kingjojo3@gmail.com>
2020-11-07 15:07:44 -0500

    Starting server

--- ftp_server.py
+++ ftp_server.py
@@ -1,1 +1,11 @@
 import socket
+import threading
+from _thread import *
+
+def Main():
+    HOST = socket.gethostbyname(socket.gethostname())
+    port = 4139
+    sckt = socket.socket(SOCKET.AF_INET, socket.SOCK_STRTEAM)
+    sckt.bind((host, port))
+# Sources:
+# https://www.geeksforgeeks.org/socket-programming-multi-threading-python/

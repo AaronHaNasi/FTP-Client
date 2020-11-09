031ed9020d0e30f929d9db64d07ec29cc8819d68
Aaron <19242692+AaronIlMentor@users.noreply.github.com>
2020-11-09 10:57:13 -0500

    List command mostly fixed
    error on ftp_server.py:
    
    line 33, in threaded
        data_connection.send(size_of_data.to_bytes(4, byteorder='big', signed=False))
    OSError: [WinError 10057] A request to send or receive data was disallowed because the socket is not connected and (when sending on a datagram socket using a sendto call) no address was supplied

--- ftp_client.py
+++ ftp_client.py
@@ -1,12 +1,12 @@
 import socket
 import os.path # to check if file exists or not
 
-
-def connect(ip_address: str):
+port = 4139
+def connect(ip_address: str, port: int):
     try:
         sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sckt.connect((ip_address, 4139))
-        print("Connection to " + ip_address + " on port 4139 succsessful.")
+        sckt.connect((ip_address, port))
+        print("Connection to " + ip_address + " on succsessful.")
         return sckt
     except socket.error:
         print('Unable to make connection')
@@ -19,26 +19,30 @@ user_input = ''
 
 
 while True: #user_input != 'QUIT' or user_input != 'Q':
-    print("""FTP Commands are as follows: 
-        \n[C]onnect: 
-        \n[L]ist 
-        \n[R]etr 
-        \n[S]tor 
-        \n[Q]uit:""")
+    print("""FTP Commands are as follows:
+        [C]onnect:
+        [L]ist
+        [R]etr
+        [S]tor
+        [Q]uit:""")
     user_input = input('\n')
     if user_input[0:6].upper() == 'CONNECT' or user_input[0].upper() == 'C':
         args = user_input.split(' ')
         ip_address = args[1]
-        sckt = connect(args[1])
+        port = int(args[2])
+        sckt = connect(ip_address, port)
         continue
     elif user_input.upper() == 'LIST' or user_input.upper() == 'L':
-        sckt.send(b"LIST")
-        data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        data_socket.connect((ip_address, 4141))
-        data_size = data_socket.recv(1024)
-        data = data_socket.recv(int.from_bytes(data_size))
-        data_socket.close()
-        print(data.decode("utf-8"))
+        sckt.send(b"LIST " + bytes(str(port), "ascii"))
+        control_data = sckt.recv(2)
+        control_data = control_data.decode('ascii')
+        if control_data == 'OK':
+            data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            data_socket.connect((ip_address, port+2))
+            data_size = data_socket.recv(4)
+            data = data_socket.recv(int.from_bytes(data_size, byteorder='big', signed=False))
+            data_socket.close()
+            print(data.decode("utf-8"))
     elif user_input.upper() == 'RETR' or user_input.upper() == 'R':
         args = user_input.split(' ')
         if sckt is None:
@@ -49,7 +53,7 @@ while True: #user_input != 'QUIT' or user_input != 'Q':
             if response is -1:
                 print("File does not exist: " + args[1])
             else:
-                data_socket.connect((ip_address, 4141))
+                data_socket.connect((ip_address, port+2))
                 file_name = data_socket.recv(255)
                 file_size = data_socket.recv(4)
                 file_data = data_socket.recv(int.from_bytes(data_size))
@@ -62,7 +66,7 @@ while True: #user_input != 'QUIT' or user_input != 'Q':
         sckt.send("STOR")
         file_exists = os.path.isfile(args[1])
         if file_exists:
-            data_socket.connect((ip_address, 4141))
+            data_socket.connect((ip_address, port+2))
             data_socket.send(bytes(args[1], "ascii"))
             data_socket.sendfile(args[1])
             data_socket.close()
--- ftp_server.py
+++ ftp_server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import os # for listing files
 from _thread import *
+import sys
 
 print_lock = threading.Lock()
 control_port = 4139
@@ -13,20 +14,28 @@ host = ''
 def threaded(client):
     while True:
         control_data = client.recv(4)
-        client.send(b'OK') 
+        control_data = control_data.decode('ascii')
+       # client.send(b'OK')
         if control_data == 'LIST':
+            print("Recieved LIST command")
             data_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             data_connection.bind((host, data_port))
             data_connection.listen(1)
+            client.send(b'OK')
+            print("Listening for data connection. Sending response...")
             data_connection.accept()
+            print("Connection accepted. Sending files...")
             files = [f for f in os.listdir('.') if os.path.isfile(f)]
             return_data = ''
             for file in files:
                 return_data += file + '\n'
             size_of_data = len(return_data.encode('utf-8'))
-            data_connection.send(size_of_data)
-            data_connection.send(data)
+            data_connection.send(size_of_data.to_bytes(4, byteorder='big', signed=False))
+            data_connection.send(data.encode(encoding='ascii'))
+            print("Data sent. Closing data connection...")
             data_connection.close()
+        else:
+            print("Unknown command recieved")
 
 
 
